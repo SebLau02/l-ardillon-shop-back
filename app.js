631e229b0299a33b8565c8edb50d1ee0cefc1185
sebastien lau <sebastien.lau2021@gmail.com>
2023-09-15 10:55:18 +0200

    variable d'environnement

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
+
+const DBPW = process.env.DB_PW;
 
 const LeurreSouple = require("./models/Leurres").model("LeurreSouple");
 const LeurreDur = require("./models/Leurres").model("LeurreDur");
@@ -20,8 +23,8 @@ app.use(cors());
 
 mongoose
 	.connect(
-		"mongodb+srv://lausebastien:4PlLxpyQYnpeBrKW@cluster0.kmwbybb.mongodb.net/",
-		{ useNewUrlParser: true, useUnifiedTopology: true }
+		`mongodb+srv://lausebastien:${DBPW}@cluster0.kmwbybb.mongodb.net/`,
+		{ useNewUrlParser: true, useUnifiedTopology: true },
 	)
 	.then(() => console.log("Connexion à MongoDB réussie !"))
 	.catch(() => console.log("Connexion à MongoDB échouée !"));
@@ -32,11 +35,11 @@ app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader(
 		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
 	);
 	res.setHeader(
 		"Access-Control-Allow-Methods",
-		"GET, POST, PUT, DELETE, PATCH, OPTIONS"
+		"GET, POST, PUT, DELETE, PATCH, OPTIONS",
 	);
 	next();
 });
